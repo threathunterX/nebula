6d3b0adc4626f189346effa753983a4ecd3efb6e
quwang <quwang@threathunter.cn>
2019-03-12 11:43:34 +0800

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
-WORK_DIR=$(dirname $(readlink -f "$0"))
+PRG="$0"
+while [ -h "$PRG" ]; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+WORK_DIR=`dirname "$PRG"`
 
 update_submodules()
 {
